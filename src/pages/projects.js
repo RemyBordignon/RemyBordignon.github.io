91e8f99ff14592c6d13d26316e0feb2559b24669
Remy <gsscg13@gmail.com>
2019-12-18 12:17:30 +1100

    Fix link

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -5,7 +5,7 @@ import { Link } from "gatsby"
 const Projects = () => {
   return (
     <Layout>
-      <h3><Link to={'https://elevator-4bb03.firebaseapp.com'}>Elevator</Link></h3>
+      <h3><a href={'https://elevator-4bb03.firebaseapp.com'}>Elevator</a></h3>
       <p>A goal setting app with a focus on habit formation by completing a minimal action towards
       your goal everyday. A react progressive web app built with Firebase.</p>
 
